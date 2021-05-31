1c2b4308c06e75c73e6e22ed4204e315185234ef
Toby Fleming <tobywf@users.noreply.github.com>
2021-05-31 13:01:02 -0700

    MSVC: Link against vcruntime

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,22 @@ pub extern "C" fn rust_eh_register_frames() {}
 #[no_mangle]
 pub extern "C" fn rust_eh_unregister_frames() {}
 
+#[cfg(all(target_os = "windows", target_env = "msvc"))]
+mod msvc {
+    // With MSVC, for some reason this is required. This is what std does:
+    // https://github.com/rust-lang/libc/blob/a016994b91a5b0dbd8f234b60af936eedb227b22/src/windows/mod.rs#L253
+
+    // Linking to "libcmt" works, but produces a large DLL and imports "KERNEL32.DLL"
+    // Linking to "msvcrt" does not work; _memcmp is not found
+    // Linking to "vcruntime" works, and produces a small DLL and imports "VCRUNTIME140.DLL" (with /NOENTRY)
+    // The dependency on the Microsoft Visual C++ Redistributable for Visual
+    // Studio 2015, 2017 and 2019 (vc_redist.x86.exe) is unfortunate, but probably
+    // the least bad option.
+
+    #[link(name = "vcruntime")]
+    extern "C" {}
+}
+
 include!(concat!(env!("OUT_DIR"), "/lookup.rs"));
 
 const INVALID_ID: i32 = 0;
